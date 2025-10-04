eb36e4902ab149e03b1a12d0b91a4407eb816c2a
YITBAREK ALEMU <160623517+jaeckult@users.noreply.github.com>
2025-10-04 11:59:58 +0300

    tanstackquery added

--- src/services/productsService.js
+++
@@ -1,8 +0,0 @@
-import { httpGet } from '../lib/http'
-
-export function fetchProducts({ page = 1, categoryId } = {}) {
-  const params = new URLSearchParams()
-  params.set('page', String(page))
-  if (categoryId) params.set('categoryId', String(categoryId))
-  return httpGet(`/products?${params.toString()}`)
-}
--- src/services/usersService.js
+++
@@ -1,9 +0,0 @@
-import { httpGet, httpPost } from '../lib/http'
-
-export function fetchUsers({ page = 1 } = {}) {
-  return httpGet(`/users?page=${page}`)
-}
-
-export function createUser(payload) {
-  return httpPost('/users', payload)
-}
